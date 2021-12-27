83f75ea2daed39d414aaa6e87952bbd6b67304e4
Scala Steward <me@scala-steward.org>
2021-12-27 14:24:07 +0000

    Update flyway-core to 8.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,7 +45,7 @@ object Dependencies {
 
   case object org {
     case object flywaydb {
-      val `flyway-core` = "org.flywaydb" % "flyway-core" % "8.2.2"
+      val `flyway-core` = "org.flywaydb" % "flyway-core" % "8.2.3"
     }
 
     case object http4s {
