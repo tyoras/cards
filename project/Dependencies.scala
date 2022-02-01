4bcd4fda8f3152ee5d7c169ebd564d8281291a7f
Scala Steward <me@scala-steward.org>
2022-02-01 21:35:23 +0100

    Update postgresql to 42.3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -58,7 +58,7 @@ object Dependencies {
     }
 
     case object postgresql {
-      val postgresql = "org.postgresql" % "postgresql" % "42.3.1"
+      val postgresql = "org.postgresql" % "postgresql" % "42.3.2"
     }
 
     case object scalacheck {
